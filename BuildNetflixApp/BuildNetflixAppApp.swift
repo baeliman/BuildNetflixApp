d61a59044d1336655fc940d6b233809bcde6ea70
Baeliman <baeliman@gmail.com>
2020-09-09 07:37:40 -0400

    Initial Commit

---
+++ BuildNetflixApp/BuildNetflixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixAppApp.swift
+//  BuildNetflixApp
+//
+//  Created by Doug Belli on 9/9/20.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildNetflixApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildNetflixApp
+//
+//  Created by Doug Belli on 9/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

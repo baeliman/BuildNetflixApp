ad370c43f7c3de1b32312985c3e4fa1d12042f26
Baeliman <baeliman@gmail.com>
2020-09-10 10:08:44 -0400

    Through three lessons

--- BuildNetflixApp/GlobalHelpers.swift
+++ BuildNetflixApp/GlobalHelpers.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 
-let exampleMoview1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300/")!)
-let exampleMoview2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/301")!)
-let exampleMoview3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/302")!)
-let exampleMoview4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/303")!)
-let exampleMoview5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/304")!)
-let exampleMoview6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/305")!)
+let exampleMoview1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300/")!, categories: ["Dystopina", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMoview2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: ["Dystopina", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMoview3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopina", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMoview4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopina", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMoview5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopina", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMoview6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/305")!, categories: ["Dystopina", "Exciting", "Suspenseful", "Sci-Fi TV"])
 
 let exampleMovies: [Movie] = [exampleMoview1, exampleMoview2, exampleMoview3, exampleMoview4, exampleMoview5, exampleMoview6]
---
+++ BuildNetflixApp/TopMoviePreview.swift
@@ -0,0 +1,74 @@
+//
+//  TopMoviePreview.swift
+//  BuildNetflixApp
+//
+//  Created by Doug Belli on 9/10/20.
+//
+
+import SwiftUI
+import KingfisherSwiftUI
+
+struct TopMoviePreview: View {
+    var movie: Movie
+    
+    var body: some View {
+        ZStack {
+            KFImage(movie.thumbnailURL)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+            
+            VStack {
+                Spacer()
+                HStack {
+                    ForEach(movie.categories, id: \.self) { category in
+                        HStack {
+                            Text(category)
+                                .font(.footnote)
+                            if !isCategoryLast(category) {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.blue)
+                                    .font(.system(size: 6))
+                            }
+                        }
+                    }
+                }
+                HStack {
+                    Spacer()
+                    SmallVerticalButton(text: "My List", isOnImage: "checkmark", isOffImage: "plus", isOn: true) {
+                        // TBD
+                    }
+                    Spacer()
+                    WhiteButton(text: "play", imageName: "play.fill") {
+                        //
+                    }
+                    .frame(width: 120)
+                    Spacer()
+                    SmallVerticalButton(text: "Info", isOnImage: "info.circle", isOffImage: "info.circle", isOn: true) {
+                        // TBD
+                    }
+                    Spacer()
+                }
+            }
+        }
+        .foregroundColor(.white)
+    }
+    
+    // Controls when the 'blue circle' is displayed
+    func isCategoryLast(_ cat:String) -> Bool {
+        let catCount = movie.categories.count
+        
+        if let index = movie.categories.firstIndex(of: cat) {
+            if index + 1 != catCount {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview(movie: exampleMoview1)
+    }
+}

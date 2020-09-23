7cbe40427f9e12f11191ec758a482421508e7bae
Baeliman <baeliman@gmail.com>
2020-09-23 06:55:27 -0400

    Two more lessons completed

---
+++ BuildNetflixApp/Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  BuildNetflixApp
+//
+//  Created by Baeliman on 9/23/20.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let graySearchBackground = Color("gray_search_background")
+    static let graySearchText = Color("gray_search_text")
+}

f1b32f3d49cc07d194f3fc90373e1bc9c6ba8201
Baeliman <baeliman@gmail.com>
2020-09-23 08:59:27 -0400

    Stopping point

--- BuildNetflixApp/Search/SearchBar.swift
+++ BuildNetflixApp/Search/SearchBar.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct SearchBar: View {
     
-    @State private var text: String = ""
+    @Binding var text: String
+    @State private var isEditing = true
+    @Binding  var isLoading: Bool
+    
     
     var body: some View {
         
@@ -26,27 +29,52 @@ struct SearchBar: View {
                 TextField("Search", text: $text)
                     .padding(7)
                     .padding(-7)
+                    .padding(.horizontal, 10)
                     .background(Color.graySearchBackground)
                     .cornerRadius(8)
                     .foregroundColor(.white)
+                    .onTapGesture(perform: {
+                        isEditing = true
+                    })
                 
-                Button(action: {
-                    // Clear text
-                }, label: {
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(.graySearchText)
+                if !text.isEmpty {
+                    if isLoading {
+                        Button(action: {
+                            text = ""
+                        }, label: {
+                            ActivityIndicator(style: .medium, animate: .constant(true))
+                                .configure( {
+                                    $0.color = .white
+                                })
+                        })
+                        .padding(.trailing, 8 )
                         .frame(width: 35, height: 35)
-                })
-                .padding(.trailing, 8)
+                        
+                    } else {
+                        Button(action: {
+                            text = ""
+                        }, label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.graySearchText)
+                                .frame(width: 35, height: 35)
+                        })
+                        .padding(.trailing, 8)
+                    }
+                }
                 
-                Button(action: {
-                    // clear text, hide both buttons, give up first-responder
-                }, label: {
-                    
-                    Text("Cancel")
-                        .foregroundColor(.white)
-                })
-                .padding(.trailing, 10)
+                
+                if isEditing {
+                    Button(action: {
+                        text = ""
+                        isEditing = false
+                        hideKeyboard()
+                    }, label: {
+                        
+                        Text("Cancel")
+                            .foregroundColor(.white)
+                    })
+                    .padding(.trailing, 10)
+                }
                 
             }
         }
@@ -57,7 +85,7 @@ struct SearchBar_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
             Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            SearchBar()
+            SearchBar(text: .constant(""), isLoading: .constant(false))
                 .padding()
         }
         

b54a087cd7a5b82c65549feabf1f328462d54b95
Baeliman <baeliman@gmail.com>
2020-09-29 07:01:27 -0400

    First part of 'coming soon' completed.

---
+++ BuildNetflixApp/ComingSoon/NotificationBar.swift
@@ -0,0 +1,42 @@
+//
+//  NotificationBar.swift
+//  BuildNetflixApp
+//
+//  Created by Baeliman on 9/29/20.
+//
+
+import SwiftUI
+
+struct NotificationBar: View {
+    
+    @Binding var showNotificationList: Bool
+    
+    var body: some View {
+        Button(action: {
+            showNotificationList = true
+        }, label: {
+            HStack {
+                Image(systemName: "bell")
+                Text("Nofifications")
+                    .bold()
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            .font(.system(size: 18, weight: .bold))
+
+        })
+        .foregroundColor(.white)
+        .padding()
+        
+    }
+}
+
+struct NotificationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            NotificationBar(showNotificationList: .constant(false))
+        }
+    }
+}
